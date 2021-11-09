e67072e097408db2f637a70b805ecff6d39d5e1b
Otavio <luiz.nascimento@innolevels.com.br>
2021-11-09 14:05:41 -0300

    iniciando api de filmes

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ FilmesAPI/Model/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Model
+{
+    public class Filme
+    {
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1,600)]
+        public int Duracao { get; set; }
+    }
+}
